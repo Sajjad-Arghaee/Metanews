5476fe8c1024b475d1a5db1b116fa2ed8a1f8524
sajjad arghaee <84894722+Sajjad-Arghaee@users.noreply.github.com>
2022-08-14 12:16:07 +0430

    Update settings.py

--- MetaNews/settings.py
+++ MetaNews/settings.py
@@ -114,12 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'arghaei1380@gmail.com'
-EMAIL_HOST_PASSWORD = 'udmijevenaahjycf'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
